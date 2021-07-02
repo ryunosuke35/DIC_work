abcc08c3b7fa6c635f989d303c186e862f3ff7cf
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-02 18:43:38 +0900

    viewファイル修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  def show
-    @user = User.find(params[:id])
-  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,6 +9,9 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
